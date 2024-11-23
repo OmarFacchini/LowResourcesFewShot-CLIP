3376210a1bc4cefae43ce7f53aad2a0e61634d28
omar facchini <omar.facchini@studenti.unitn.it>
2024-11-23 14:56:10 +0100

    added label breaking and preserving concept to eurosat dataset, added label mapping for eurosat and added file for image generation for that dataset too

--- datasets/eurosat.py
+++ datasets/eurosat.py
@@ -33,10 +33,11 @@ class EuroSAT(DatasetBase):
         
         self.template = template
 
-        train, val, test = OxfordPets.read_split(self.split_path, self.image_dir)
+        #train, val, test = OxfordPets.read_split(self.split_path, self.image_dir)
+        train, val, test = self.read_split(self.split_path, self.image_dir)
         n_shots_val = min(num_shots, 4)
-        val = self.generate_fewshot_dataset(val, num_shots=n_shots_val)
-        train = self.generate_fewshot_dataset(train, num_shots=num_shots)
+        #val = self.generate_fewshot_dataset(val, num_shots=n_shots_val)
+        #train = self.generate_fewshot_dataset(train, num_shots=num_shots)
         
         super().__init__(train_x=train, val=val, test=test)
     
@@ -52,3 +53,70 @@ class EuroSAT(DatasetBase):
             )
             dataset_new.append(item_new)
         return dataset_new
+    
+    @staticmethod
+    def read_split(filepath, path_prefix):
+        def _convert(items, train=False):
+            out = []
+            for impath, label, classname in items:
+                # get folder(class) and filename
+                folder, filename = os.path.split(impath)
+
+                # number of filename, eg annualcrop_181 -> 181
+                number = filename.split("_")[1].split(".")[0] 
+
+                # path of original image
+                # path_prefix = dataset/eurosat/2750
+                # impath = dataset/eurosat/2750/AnnualCrop/annualCrop_1.png
+                impath = os.path.join(path_prefix, impath) 
+
+                # insert the original image
+                item = Datum(
+                    impath=impath,
+                    label=int(label),
+                    classname=classname,
+                    imgtype="original"
+                )
+                out.append(item)
+
+                # concept of label preserving and breaking is applied only to training
+                if train:
+                    # dataset/dataset/eurosat/2750/label_preserving/AnnualCrop
+                    preserving_dir_path = os.path.join(path_prefix, "label_preserving", folder)
+                    
+                    # dataset/dataset/eurosat/2750/label_breaking/AnnualCrop
+                    breaking_dir_path = os.path.join(path_prefix, "label_breaking", folder)
+
+                    # for each original image, take its preserving and its breaking images
+                    for path in [preserving_dir_path, breaking_dir_path]:
+                        # select imgtype based on path i am currently in
+                        imgtype = "preserving" if "preserving" in path else "breaking"
+
+                        # dataset/dataset/eurosat/2750/label_*/AnnualCrop/1
+                        img_path = os.path.join(path, number)
+
+                        '''check if folder exists, should always be true as we generate samples from train set
+                        and enter here only if we are training'''
+                        if os.path.exists(img_path):
+                            # loop on all samples generated for the original image
+                            # 1.png, 2.png.....
+                            for filename in os.listdir(img_path):
+                                # dataset/dataset/eurosat/2750/label_*/1.png
+                                sample_path = os.path.join(img_path, filename)
+                                
+                                item = Datum(
+                                    impath=sample_path,
+                                    label=int(label),
+                                    classname=classname,
+                                    imgtype=imgtype
+                                )
+                                out.append(item)
+            return out
+        
+        print(f'Reading split from {filepath}')
+        split = read_json(filepath)
+        train = _convert(split['train'], True)
+        val = _convert(split['val'])
+        test = _convert(split['test'])
+
+        return train, val, test
